f9a6e0053d07cdbcebf4b3e55082ad26aad73927
kyumin1227 <kyumin12271227@gmail.com>
2024-04-24 01:11:15 +0900

    Style: Change Logo

--- src/pages/loginPage/index.tsx
+++ src/pages/loginPage/index.tsx
@@ -26,10 +26,10 @@ const GoogleLogin = () => {
   return (
     <Box height="100vh" width="100%" bgcolor="#5C95FF" display={"flex"} alignItems={"center"} justifyContent={"center"}>
       <Grid>
-        <Grid display={"flex"} alignItems={"center"} marginBottom="50%">
-          <img src="./logo/logo_remove_text.png" alt="" width="100px" />
+        <Grid display={"flex"} alignItems={"center"} marginBottom="20%">
+          <img src="./logo2/logo2_bg_remove.png" alt="" width="100px" />
           <Typography color={"white"} variant="h3" style={{ fontFamily: "Madimi One" }} marginLeft="15px">
-            GSC-FP
+            GSC - FP
           </Typography>
         </Grid>
         <Grid display={"flex"} justifyContent={"center"} marginBottom="30%">
